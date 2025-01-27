a51ab7c8dcbbc322fff18fe771aa44f2917bd0c4
Muhammad Abu Bakar <161414816+abubakar2110@users.noreply.github.com>
2025-01-27 22:06:55 +0500

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.12.35514.174 d17.12
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "ConsoleApplication1", "ConsoleApplication1\ConsoleApplication1.vcxproj", "{51ED0874-F1C2-4E4F-BBC5-D5355104A320}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Debug|x64.ActiveCfg = Debug|x64
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Debug|x64.Build.0 = Debug|x64
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Debug|x86.ActiveCfg = Debug|Win32
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Debug|x86.Build.0 = Debug|Win32
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Release|x64.ActiveCfg = Release|x64
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Release|x64.Build.0 = Release|x64
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Release|x86.ActiveCfg = Release|Win32
+		{51ED0874-F1C2-4E4F-BBC5-D5355104A320}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
